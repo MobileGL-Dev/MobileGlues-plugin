c88243ef3b94f11cab9fe4cd54c8617c7aa1f738
Capslock800000 <mmzyd@outlook.com>
2026-07-11 21:29:33 +0800

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
             resValue("string","app_name","OptimizedMG")
 
             manifestPlaceholders["des"] = "OptimizedMG (OpenGL 4.0, 1.17+)"
-            manifestPlaceholders["renderer"] = "optimizedMG:liboptimizedmg.so:liboptimizedmg.so"
+            manifestPlaceholders["renderer"] = "OptimizedMG:liboptimizedmg.so:liboptimizedmg.so"
 
             manifestPlaceholders["minMCVer"] = "1.17"
             manifestPlaceholders["maxMCVer"] = "" //为空则不限制 No restriction if empty
