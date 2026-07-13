29fa017730d5993a05cb85a35409cec407f22ac1
Capslock800000 <mmzyd@outlook.com>
2026-07-13 10:40:03 +0800

    a

--- app/src/main/java/com/fcl/plugin/mobileglues/utils/Constants.kt
+++ app/src/main/java/com/fcl/plugin/mobileglues/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.capslock800000.optimizedmg.utils
 import android.os.Environment
 
 object Constants {
-    val MG_DIRECTORY: String = "${Environment.getExternalStorageDirectory().absolutePath}/MG"
+    val MG_DIRECTORY: String = "${Environment.getExternalStorageDirectory().absolutePath}/OMG"
 
     val CONFIG_FILE_PATH: String = "$MG_DIRECTORY/config.json"
 
